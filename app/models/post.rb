a66b02e41e153ae87b9840513d04fea363d1c877
Lisa Lee <snickers495@gmail.com>
2017-12-03 20:08:31 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   validates :name, presence: true
   validates :content, presence: true
 
-  belongs_to :user 
-  has_many :tags, through: :post_tag 
+  belongs_to :user
+  has_many :tags, through: :post_tag
   has_many :post_tags
 end
