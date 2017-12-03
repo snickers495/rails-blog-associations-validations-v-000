8778d3067d065dae425ac3548a9de837d1469e0e
Lisa Lee <snickers495@gmail.com>
2017-12-03 20:08:16 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ActiveRecord::Base
   validates :name, presence: true
   validates :content, presence: true
+
+  belongs_to :user 
+  has_many :tags, through: :post_tag 
+  has_many :post_tags
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 RailsBlog::Application.routes.draw do
 
+  resources :post_tags
+
   resources :tags
 
   resources :users
